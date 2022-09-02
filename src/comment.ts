82bd866464235f4c6e2b530a11ac2ea6a87ec972
= <jptech@johnpuaoi.tech>
2022-09-02 11:15:21 -1000

    feat(settings): set config to allow customizing of pop doc comment template and the keys that insert a PopDoc comment.

--- src/comment.ts
+++ src/comment.ts
@@ -1,12 +1,18 @@
 import * as vscode from 'vscode';
 
-const commentTemplate =
-  '<!-- @PopDoc \n ### Component Name \n This is a short description of how to use the component. \n \n ### Props \n - example list of props \n \n ### Emits \n - example list of events \n \n ### Slots \n - example list of slots \n \n ### Examples \n ```vue \n<template>\n <div>Test</div> \n</template> \n ``` \n \n --> \n';
+// const commentTemplate =
+//   '<!-- @PopDoc \n ### Component Name \n This is a short description of how to use the component. \n \n ### Props \n - example list of props \n \n ### Emits \n - example list of events \n \n ### Slots \n - example list of slots \n \n ### Examples \n ```vue \n<template>\n <div>Test</div> \n</template> \n ``` \n \n --> \n';
 
 export const createPopDocComment = () => {
   return vscode.commands.registerCommand(
     'nuxtcomponentpopdoc.createPopDocComment',
     () => {
+      // Retrieving settings here to ensure that changes to settings are reflected in the extension.
+      // Get commentTemplate from settings
+      const commentTemplate = vscode.workspace
+        .getConfiguration('nuxtcomponentpopdoc')
+        .get('commentTemplate') as string;
+
       const editor = vscode.window.activeTextEditor;
 
       if (!editor) {
@@ -22,10 +28,21 @@ export const createPopDocComment = () => {
       }
 
       const selection = editor.selection;
-      const text = editor.document.getText(selection);
-      editor.edit((editBuilder) => {
-        editBuilder.replace(selection, commentTemplate);
-      });
+      editor
+        .edit((editBuilder) => {
+          editBuilder.replace(selection, commentTemplate);
+        })
+        .then(() => {
+          // Select the text on line 2, starting at char 5 and ending at char 19
+          const line2 = new vscode.Position(1, 5);
+          const line2End = new vscode.Position(1, 19);
+
+          // Create a new selection
+          const newSelection = new vscode.Selection(line2, line2End);
+
+          // And finally, set the new selection
+          editor.selection = newSelection;
+        });
     }
   );
 };
@@ -33,6 +50,12 @@ export const createPopDocComment = () => {
 // A function that watches current file and line for changes, if the user types <pd in the current line, trigger nuxtcomponentpopdoc.createPopDocComment
 export const watchForPopDocCommentKeypress = () => {
   return vscode.workspace.onDidChangeTextDocument((event) => {
+    // Retrieving settings here to ensure that changes to settings are reflected in the extension.
+    // Get commentTemplate from settings
+    const commentInsertKeys = vscode.workspace
+      .getConfiguration('nuxtcomponentpopdoc')
+      .get('commentInsertKeys') as string;
+
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
       return;
@@ -47,11 +70,18 @@ export const watchForPopDocCommentKeypress = () => {
     const lineText = line.text;
 
     console.log('Line Text: ', lineText);
-    if (lineText.includes('<pd')) {
+    if (lineText.includes(commentInsertKeys)) {
       // Remove the <pd from the line, then trigger the command
       editor
         .edit((editBuilder) => {
-          editBuilder.replace(line.range, '');
+          // Replace just the <pd and not any other text on the line with nothing
+          editBuilder.replace(
+            new vscode.Range(
+              new vscode.Position(selection.active.line, 0),
+              new vscode.Position(selection.active.line, 3)
+            ),
+            ''
+          );
         })
         .then(() => {
           vscode.commands.executeCommand(
