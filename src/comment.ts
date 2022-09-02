0a1c3190cb0491ae9be318478e6fd8262ae230ac
= <jptech@johnpuaoi.tech>
2022-09-02 10:36:02 -1000

    feat(comment.ts): on <pd keypress insert pop doc comment template

--- src/comment.ts
+++ src/comment.ts
@@ -1,7 +1,63 @@
 import * as vscode from 'vscode';
 
-//
+const commentTemplate =
+  '<!-- @PopDoc \n ### Component Name \n This is a short description of how to use the component. \n \n ### Props \n - example list of props \n \n ### Emits \n - example list of events \n \n ### Slots \n - example list of slots \n \n ### Examples \n ```vue \n<template>\n <div>Test</div> \n</template> \n ``` \n \n --> \n';
+
 export const createPopDocComment = () => {
-  const commentTemplate =
-    '<!-- @popDoc \n ### Component Name \n \n ### Description \n \n ### Props \n \n ### Events \n \n ### Slots \n \n ### Examples \n \n -->';
+  return vscode.commands.registerCommand(
+    'nuxtcomponentpopdoc.createPopDocComment',
+    () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (!editor) {
+        return;
+      }
+
+      // Check the current file is a vue file
+      if (editor.document.languageId !== 'vue') {
+        vscode.window.showErrorMessage(
+          'Nuxt Component Pop Doc: Please open a vue file to use this command.'
+        );
+        return;
+      }
+
+      const selection = editor.selection;
+      const text = editor.document.getText(selection);
+      editor.edit((editBuilder) => {
+        editBuilder.replace(selection, commentTemplate);
+      });
+    }
+  );
+};
+
+// A function that watches current file and line for changes, if the user types <pd in the current line, trigger nuxtcomponentpopdoc.createPopDocComment
+export const watchForPopDocCommentKeypress = () => {
+  return vscode.workspace.onDidChangeTextDocument((event) => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    console.log('watchForPopDocComment');
+    const document = editor.document;
+    console.log('Doc: ', document);
+    const selection = editor.selection;
+    console.log('Selection: ', selection);
+    const line = document.lineAt(selection.active.line);
+    console.log('Line: ', line);
+    const lineText = line.text;
+
+    console.log('Line Text: ', lineText);
+    if (lineText.includes('<pd')) {
+      // Remove the <pd from the line, then trigger the command
+      editor
+        .edit((editBuilder) => {
+          editBuilder.replace(line.range, '');
+        })
+        .then(() => {
+          vscode.commands.executeCommand(
+            'nuxtcomponentpopdoc.createPopDocComment'
+          );
+        });
+    }
+  });
 };
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { createPopDocComment, watchForPopDocCommentKeypress } from './comment';
 import { hoverProvider } from './hover';
 
 // this method is called when your extension is activated
@@ -10,7 +11,11 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Nuxt Pop Doc Activated');
   // Add subscriptions
-  context.subscriptions.push(hoverProvider());
+  context.subscriptions.push(
+    hoverProvider(),
+    createPopDocComment(),
+    watchForPopDocCommentKeypress()
+  );
 }
 
 // this method is called when your extension is deactivated
